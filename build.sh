77b1ccc87181f83dd3c118f189525eb287b0b5ca
Uri <hello@uriherrera.com>
2019-06-02 19:21:59 -0500

    update dependency name

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ TRAVIS_BRANCH=$2
 
 apt-get -qq -y update > /dev/null
 apt-get -qq -y install wget patchelf file libcairo2 git > /dev/null
-apt-get -qq -y install busybox-static kde-baseapps-bin > /dev/null
+apt-get -qq -y install busybox-static kdialog > /dev/null
 
 wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 wget -q https://gitlab.com/nitrux/tools/raw/master/copier
@@ -39,7 +39,6 @@ mkdir -p appdir/bin
 # -- Copy binaries and its dependencies to appdir.
 
 ./copier kdialog appdir
-
 
 # -- Include znx.
 
